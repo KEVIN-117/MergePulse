39d8514b0bbbc710e2c1cb97358814ced4b12f03
KEVIN-117 <krodriguezcondori34@gmail.com>
2026-02-26 11:45:06 -0400

    feat: All apps running

--- apps/backend/src/organizations/dto/create-organization.dto.ts
+++ apps/backend/src/organizations/dto/create-organization.dto.ts
@@ -1,4 +1,13 @@
+import { IsString, MinLength } from "class-validator";
+
 export class CreateOrganizationDto {
+    @IsString()
+    @MinLength(3)
     name: string;
+
+    @IsString()
+    @MinLength(3)
     slug: string;
+
+    githubInstallationId: string;
 }
